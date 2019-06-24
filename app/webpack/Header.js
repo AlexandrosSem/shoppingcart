3e34419fd75b8dc37026713da3a00d61c6c556ef
Alexandros <alexandrossemelidis@gmail.com>
2019-06-24 21:49:08 +0100

    Introduction of product global counter on header.

--- app/webpack/Header.js
+++ app/webpack/Header.js
@@ -1,14 +1,24 @@
 export default {
-    props: ['value'], // value is set by v-model
+    props: ['value', 'params'], // value is set by v-model
     data: function() {
         return {
-            PageNow: this.value // Make a "copy" of the parent variable
+            PageNow: this.value, // Make a "copy" of the parent variable
+            ProductsInfoOnCart: this.params.ProductsInfoOnCart
         };
     },
     template: `<nav class="navbar is-light">
         <a class="navbar-item" v-bind:class="{ 'is-active':(PageNow === 'Products') }" href="#" v-on:click="SetPage('Products')">Products</a>
-        <a class="navbar-item" v-bind:class="{ 'is-active':(PageNow === 'Cart') }" href="#" v-on:click="SetPage('Cart');">Cart</a>
+        <a class="navbar-item" v-bind:class="{ 'is-active':(PageNow === 'Cart') }" href="#" v-on:click="SetPage('Cart');">({{CountAllProductsOnCard}}) Cart</a>
     </nav>`,
+    computed: {
+        CountAllProductsOnCard() {
+			let tTotalCount = 0;
+			this.ProductsInfoOnCart.forEach(function(pEl) {
+				tTotalCount += pEl.QuantityOnCart;
+			});
+			return tTotalCount;
+		},
+    },
     methods: {
         SetPage: function(pPage) {
             this.PageNow = pPage;
