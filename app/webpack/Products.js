40f029a8871b2c625b50377f89e2c0c2efe4e396
Alexandros <alexandrossemelidis@gmail.com>
2019-07-01 21:38:24 +0100

    Add everything to cart functionality.

--- app/webpack/Products.js
+++ app/webpack/Products.js
@@ -2,6 +2,9 @@ import Product from './Product';
 export default {
     props: ['params'],
     template: `<div>
+        <div>
+        <button class="button is-success" v-bind:disabled="CheckIfAnyProductStockLeft" v-on:click="AddAllProductsToCart()">Add all products to cart</button>
+        </div>
         <div is="Product" v-for="Product in Products" v-bind:params="{Products, ProductsInfoOnCart, ProductsIndex, Product}" v-bind:key="Product.Id"></div>
     </div>`,
     data () {
@@ -10,6 +13,47 @@ export default {
             ProductsInfoOnCart: this.params.ProductsInfoOnCart,
             ProductsIndex: this.params.ProductsIndex
         };
+    },
+    computed: {
+        CheckIfAnyProductStockLeft() {
+            let tResult = true;
+            const tProducts = this.Products;
+            for (let i = 0, l = tProducts.length; i < l; i++) {
+                if (tProducts[i].StockQuantity > 0) {
+                    tResult = false;
+                    break;
+                }
+            }
+            return tResult;
+        }
+    },
+    methods: {
+        AddAllProductsToCart() {
+            let tProducts = this.Products;
+            let tProductsInfoOnCart = this.ProductsInfoOnCart;
+            tProducts.forEach(function(pEl) {
+                const tCurrentId = pEl.Id;
+                const tTargetIndex = tProductsInfoOnCart.findIndex(function(pEl) {
+                    return tCurrentId === pEl.Id;
+                });
+                if (tTargetIndex > -1) {
+                    tProductsInfoOnCart[tTargetIndex].QuantityOnCart += pEl.StockQuantity;
+                } else {               
+                    const tObjProductOnCart = {
+                        Id: pEl.Id,
+                        Title: pEl.Title,
+                        SubTitle: pEl.SubTitle,
+                        ImageURL: pEl.ImageURL,
+                        Price: pEl.Price,
+                        Description: pEl.Description,
+                        StockQuantity: 0,
+                        QuantityOnCart: pEl.StockQuantity
+                    };
+                    tProductsInfoOnCart.push(tObjProductOnCart);
+                }
+                pEl.StockQuantity = 0;
+            });
+        }
     },
     components: {
         Product
